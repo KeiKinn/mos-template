1295c4b6d7e89343e5582355544a16740d949fe7
KeiKinn <jx589g@gmail.com>
2024-12-15 20:12:22 +0100

    vmos initial

--- render_vmos.py
+++ render_vmos.py
@@ -2,22 +2,20 @@
 """Generate forms for human evaluation."""
 
 from jinja2 import FileSystemLoader, Environment
-from utils import QuestionGenerator_MOS as QuestionGenerator
+from utils import QuestionGenerator_FMOS as QuestionGenerator
 
 
 def main():
     """Main function."""
     loader = FileSystemLoader(searchpath="./templates")
     env = Environment(loader=loader)
-    template = env.get_template("mos.html.jinja2")
-    
-
+    template = env.get_template("vmos.html.jinja2")
 
     html = template.render(
         page_title="MOS Experiment Form 1",
         form_id=1,
-        type="q-MOS",
-        questions = QuestionGenerator("filelist/audio_order.csv").questions
+        type="v-MOS",
+        questions = QuestionGenerator("filelist/filelist.csv").questions
     )
     # write html into file
     with open("rendered_mos.html", "w") as f:
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import csv
+import re
+import random
 
 class QuestionGenerator:
     def __init__(self, csv_file_path):
@@ -50,6 +52,45 @@ class QuestionGenerator_PMOS(QuestionGenerator):
                     "title": f"Question {idx}:   {emotion} Speech",  # Dynamically generate title
                     "audio_paths": [f"{f1}", f"{f2}"],  # Assume all files are in 'wavs/' directory
                     "name": f"q{idx}"  # Unique identifier for the question
+                }
+                questions.append(question)
+        return questions
+
+# MOS with special function for filename processing
+class QuestionGenerator_FMOS(QuestionGenerator):
+    def __init__(self, csv_file_path, random_p=3):
+        self.random_p = random_p
+        self.regexpattern = r'dia(\d+)_utt(\d+)_R\[(\w+)\]_GT\[(\w+)\]'
+        self.csv_file_path = csv_file_path
+        self.questions = self.generate_questions()
+
+    def generate_questions(self):
+        questions = []
+        filepath_list = []
+        with open(self.csv_file_path, mode='r', encoding='utf-8') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            for row in csv_reader:
+                filepath_list.append(row['filepath'])
+
+            for idx, filepath in enumerate(filepath_list, start=1):
+                filename = filepath.split('/')[-1]
+                match = re.match(self.regexpattern, filename)
+                if match:
+                    dia = match.group(1)
+                    utt = match.group(2)
+                    R = match.group(3)
+                    GT = match.group(4)
+                    mark = f"d{dia}_u{utt}_R{R[:2]}_G{GT[:2]}"
+                else:
+                    print("No match found")
+                keywords = [R, GT]
+                random.shuffle(keywords)
+                question = {
+                    "id": idx,
+                    "title": f"Question {idx}",  # Dynamically generate title
+                    "media_path": f"{filepath}",  # Assume all files are in 'wavs/' directory
+                    "name": f"v{idx}-{mark}",  # Unique identifier for the question
+                    "keywords": keywords
                 }
                 questions.append(question)
         return questions
